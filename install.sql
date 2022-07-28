db50d4c523203de1b1890b5d446dd87a3b11f83f
Khushi Dayal <khushidayal25.94@gmail.com>
2022-07-28 17:18:07 +0800

    Add constraints and comments

--- install.sql
+++ install.sql
@@ -1,6 +1,6 @@
 -- create the database
 CREATE DATABASE toDoList;
--- use the database
+-- select and use the database
 USE toDoList;
 
 -- create tables
@@ -10,18 +10,21 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `date` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  CONSTRAINT uc_user UNIQUE (id, name, email)
+  CONSTRAINT uc_user UNIQUE (name, email)
 ) ENGINE=InnoDB;
 
+-- one to many relationships with users
 CREATE TABLE `lists` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
   `name` varchar(100) NOT NULL,
   `date` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id)
+  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id),
+  CONSTRAINT uc_user_list UNIQUE (user_id, name)
 ) ENGINE=InnoDB;
 
+-- one to many relationships with lists
 CREATE TABLE `listItems` (
   `id` int NOT NULL AUTO_INCREMENT,
   `list_id` int NOT NULL,
@@ -30,9 +33,11 @@ CREATE TABLE `listItems` (
   `priority` ENUM('low', 'medium', 'high'),
   `date` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  CONSTRAINT fk_list_id FOREIGN KEY (list_id) REFERENCES lists(id)
+  CONSTRAINT fk_list_id FOREIGN KEY (list_id) REFERENCES lists(id),
+  CONSTRAINT uc_list_item UNIQUE (list_id, name)
 ) ENGINE=InnoDB;
 
+-- one to many relationships with listItems
 CREATE TABLE `listItemComments` (
   `id` int NOT NULL AUTO_INCREMENT,
   `list_item_id` int NOT NULL,
